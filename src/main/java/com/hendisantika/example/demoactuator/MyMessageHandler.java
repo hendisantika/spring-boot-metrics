af81f4748ae34b6231aae6378c2bd1f6d2542b06
Hendi Santika <hendisantika@gmail.com>
2017-10-10 08:56:00 +0700

    #2 - Added Actuator Libs

---
+++ src/main/java/com/hendisantika/example/demoactuator/MyMessageHandler.java
@@ -0,0 +1,14 @@
+package com.hendisantika.example.demoactuator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : demo-actuator
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/10/17
+ * Time: 08.54
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyMessageHandler {
+}
