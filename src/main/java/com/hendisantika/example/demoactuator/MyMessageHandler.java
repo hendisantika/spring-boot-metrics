7a9e3cfaa33126ef86a7c2458470e3d00ca0a252
Hendi Santika <hendisantika@gmail.com>
2017-10-10 08:57:00 +0700

    #3 - Added Message Handler

--- src/main/java/com/hendisantika/example/demoactuator/MyMessageHandler.java
+++ src/main/java/com/hendisantika/example/demoactuator/MyMessageHandler.java
@@ -1,5 +1,8 @@
 package com.hendisantika.example.demoactuator;
 
+import org.springframework.messaging.MessageHandler;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : demo-actuator
@@ -10,5 +13,13 @@ package com.hendisantika.example.demoactuator;
  * Time: 08.54
  * To change this template use File | Settings | File Templates.
  */
-public class MyMessageHandler {
+
+@Service
+public class MyMessageHandler implements MessageHandler {
+    public void handleMessage(org.springframework.messaging.Message<?> message) throws org.springframework.messaging.MessagingException {
+        for (String headerKey : message.getHeaders().keySet()) {
+            System.out.println("Header: "+headerKey+"="+message.getHeaders().get(headerKey));
+        }
+        System.out.println("Payload: "+message.getPayload());
+    }
 }
