c4b33d0fbcfe7f0ebeb3e97bde2eaaf1a8bf84e4
Hendi Santika <hendisantika@gmail.com>
2017-10-10 08:54:16 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/example/demoactuator/DemoActuatorApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.example.demoactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoActuatorApplication.class, args);
+	}
+}
